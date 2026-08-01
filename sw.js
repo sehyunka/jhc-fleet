fd8173809ef685c42ad52091f4e0d8403afcea67
sehyunka <sehyun.ka@gmail.com>
2026-08-01 19:39:42 +0900

    Restrict staff access to assigned vehicles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jhc-fleet-command-v4';
+const CACHE = 'jhc-fleet-command-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './firebase-config.js', './icon-192.png', './icon-512.png', './logo-h.png', './fleet-login-bg.png'];
 
 self.addEventListener('install', e => {
