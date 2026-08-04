0be71b0110d611110023a06be51a049a6e444808
sehyunka <sehyun.ka@gmail.com>
2026-08-04 15:30:56 +0900

    Reorder admin operators

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jhc-fleet-command-v10';
+const CACHE = 'jhc-fleet-command-v11';
 const ASSETS = ['./', './index.html', './manifest.json', './firebase-config.js', './icon-192.png', './icon-512.png', './logo-h.png', './fleet-login-bg.png'];
 
 self.addEventListener('install', e => {
